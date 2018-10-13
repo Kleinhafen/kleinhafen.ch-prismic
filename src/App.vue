6c7e236f4bd1921e183c581cb43f258b815a06b3
Vlad-Ștefan Harbuz <vlad@vladh.net>
2018-10-13 19:10:25 +0200

    Add some nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
 #app
+  .nav
+    span ABOUT US
   router-view
 </template>
 
@@ -13,6 +15,8 @@ export default {
 <style lang="stylus">
 @import "./assets/css/prismic-edit-button.css"
 
+color-kh = #67f1af
+
 html, body
   margin 0
   padding 0
@@ -25,4 +29,19 @@ h1, h2, h3, h4, h5, h6
 
 h1
   font-size 3rem
+
+.nav
+  position fixed
+  top 0
+  left 0
+  right 0
+  z-index 100
+  display flex
+  align-items center
+  justify-content center
+  height 2.5rem
+  background color-kh
+  font-family Europa, Helvetica, sans-serif
+  text-transform uppercase
+  font-size 0.9rem
 </style>

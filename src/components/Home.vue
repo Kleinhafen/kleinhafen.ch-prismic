d6289696b19ec6dd4b9346259af9e2378839cb5b
Vlad-Ștefan Harbuz <vlad@vladh.net>
2018-10-13 13:18:28 +0200

    Add plans

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,19 @@
 <template lang='pug'>
 .home
   h1 {{$prismic.richTextAsPlain(home.page_title)}}
-  p {{$prismic.richTextAsPlain(home.page_summary)}}
+  prismic-rich-text(:field='home.page_summary')
   h2 {{$prismic.richTextAsPlain(home.plans_title)}}
+  .plans
+    .plan(v-for='plan in plans')
+      img(:src='plan.image.url' height='100' width='100')
+      h3 {{$prismic.richTextAsPlain(plan.name)}}
+      prismic-rich-text(:field='plan.description')
+      button
+        prismic-rich-text(:field='plan.price_text')
   h2 {{$prismic.richTextAsPlain(home.rooms_title)}}
-  p {{$prismic.richTextAsPlain(home.rooms_summary)}}
+  prismic-rich-text(:field='home.rooms_summary')
   h2 {{$prismic.richTextAsPlain(home.about_title)}}
-  p {{$prismic.richTextAsPlain(home.about_summary)}}
+  prismic-rich-text(:field='home.about_summary')
 </template>
 
 <script>
@@ -16,15 +23,8 @@ export default {
   name: 'Home',
   data() {
     return {
-      home: {
-        page_title: null,
-        page_summary: null,
-        plans_title: null,
-        rooms_title: null,
-        rooms_summary: null,
-        about_title: null,
-        about_summary: null,
-      }
+      home: {},
+      plans: [],
     }
   },
   methods: {
@@ -32,17 +32,20 @@ export default {
       this.$router.push({name: 'not-found'})
     },
     getContent(uid) {
-      this.$prismic.client.getSingle('home').then(doc => {
-        if (!doc) { return this.panic() }
-        this.home = doc.data
+      Promise.all([
+        this.$prismic.client.getSingle('home'),
+        this.$prismic.client.query(
+          Prismic.Predicates.at('document.type', 'plan')
+        ),
+      ]).then(([homeDoc, planDocs]) => {
+        if (!homeDoc || !planDocs) { return this.panic() }
+        this.home = homeDoc.data
+        this.plans = planDocs.results.map((d) => d.data)
       })
     }
   },
   created() {
     this.getContent()
-    this.$prismic.client.query(Prismic.Predicates.at('document.type', 'event')).then(docs => {
-      console.log(docs)
-    })
   },
   beforeRouteUpdate(to, from, next) {
     this.getContent()

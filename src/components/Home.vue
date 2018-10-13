e28696691cd96ec03cca4958f8f30beb01b9343d
Vlad-Ștefan Harbuz <vlad@vladh.net>
2018-10-13 18:59:28 +0200

    Add hero

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template lang='pug'>
 .home
+  .hero
+    .hero__video-container
+      .hero__overlay
+      iframe(src='//player.vimeo.com/video/170963719?api=1&background=1')
+    .hero__content
+      .hero__logo
+        img(src='../assets/img/kh-logo.png')
   h1 {{$prismic.richTextAsPlain(home.page_title)}}
   prismic-rich-text(:field='home.page_summary')
   h2 {{$prismic.richTextAsPlain(home.plans_title)}}
@@ -28,6 +35,60 @@
   h2 {{$prismic.richTextAsPlain(home.about_title)}}
   prismic-rich-text(:field='home.about_summary')
 </template>
+
+<style lang='stylus'>
+.home
+  .hero
+    position relative
+    max-height 80vh
+    overflow hidden
+    background #666
+    .hero__content
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom 0
+      z-index 30
+      display flex
+      align-items center
+      justify-content center
+      .hero__logo
+        width 20rem
+        height 20rem
+        max-width 70%
+        max-height 70%
+        text-align center
+        img
+          height auto
+          max-width 100%
+          max-height 100%
+    .hero__video-container
+      position relative
+      padding-bottom 56.25%
+      height 0
+      .hero__overlay
+        background rgba(0,0,0,0.1)
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom 0
+        z-index 20
+      iframe
+        display block
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom 0
+        z-index 10
+        width 100%
+        height 100%
+        margin 0
+        padding 0
+        border 0
+</style>
 
 <script>
 import Prismic from 'prismic-javascript'
@@ -71,6 +132,3 @@ export default {
   }
 }
 </script>
-
-<style lang='stylus'>
-</style>
